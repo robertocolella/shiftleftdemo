7d37be7fa90170b78dfb979ee93c91a261e08f52
robertocolella <robertocolella@gmail.com>
2024-10-21 22:32:15 +0200

    modificato:             files/checkGit.sh

--- files/checkGit.sh
+++ files/checkGit.sh
@@ -4,9 +4,9 @@ REPOOUTPUT=$(curl -k -u $TL_USER:$TL_PASS \
   -H 'Content-Type: application/json' \
   https://$TL_CONSOLE/api/v1/coderepos?id=se-cloud-emea%2Fevil.petclinic&limit=15&offset=0&project=Central+Console&reverse=true&sort=vulnerabilityRiskScore)
 
-sed -n -e 's/^.*\(vulnerabilitiesCount\)/\1/p' | cut -f1 -d, | cut -f2- -d: > output.txt
-# VULN=$(echo $REPOOUTPUT | sed -n -e 's/^.*\(vulnerabilitiesCount\)/\1/p' | cut -f1 -d, | cut -f2- -d:)
-VULN=$(cat output.txt)
+# sed -n -e 's/^.*\(vulnerabilitiesCount\)/\1/p' | cut -f1 -d, | cut -f2- -d: > output.txt
+VULN=$(echo $REPOOUTPUT | sed -n -e 's/^.*\(vulnerabilitiesCount\)/\1/p' | cut -f1 -d, | cut -f2- -d:)
+# VULN=$(cat output.txt)
 
 if (( $VULN == 0 )); then
    echo "No Code Repo Vulnerabilities!"
