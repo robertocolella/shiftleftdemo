cc14fc3d1b48f0d95c4417ac25f31a86128725d6
robertocolella <robertocolella@gmail.com>
2024-10-21 22:22:49 +0200

    modificato:             files/checkGit.sh

--- files/checkGit.sh
+++ files/checkGit.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -l
+TL_CONSOLE="app0.prismacloud.io"
 REPOOUTPUT=$(curl -k -u $TL_USER:$TL_PASS \
   -H 'Content-Type: application/json' \
   https://$TL_CONSOLE/api/v1/coderepos?id=se-cloud-emea%2Fevil.petclinic&limit=15&offset=0&project=Central+Console&reverse=true&sort=vulnerabilityRiskScore)
